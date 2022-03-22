e66f289bdff2c0e790485f0662bb2d05d6accd66
Hakam Qalalwi <73319756+HakamQalalwi@users.noreply.github.com>
2022-03-23 01:35:46 +0300

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,327 @@
+﻿using System.Text;
+
+namespace CreatePdfSendAsMail
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.btnMinimized = new System.Windows.Forms.Button();
+            this.btnMaximized = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxEmailAddress = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxSubject = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbxPdfFileName = new System.Windows.Forms.TextBox();
+            this.rtbMailMessage = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rtbPdfText = new System.Windows.Forms.RichTextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.dragControl1 = new CreatePdfSendAsMail.DragControl();
+            this.pnlHeader.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.pnlHeader.Controls.Add(this.btnMinimized);
+            this.pnlHeader.Controls.Add(this.btnMaximized);
+            this.pnlHeader.Controls.Add(this.label1);
+            this.pnlHeader.Controls.Add(this.btnClose);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(822, 40);
+            this.pnlHeader.TabIndex = 0;
+            // 
+            // btnMinimized
+            // 
+            this.btnMinimized.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnMinimized.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnMinimized.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnMinimized.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.btnMinimized.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.btnMinimized.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimized.Font = new System.Drawing.Font("MingLiU-ExtB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinimized.ForeColor = System.Drawing.Color.Black;
+            this.btnMinimized.Location = new System.Drawing.Point(640, 0);
+            this.btnMinimized.Name = "btnMinimized";
+            this.btnMinimized.Size = new System.Drawing.Size(62, 40);
+            this.btnMinimized.TabIndex = 8;
+            this.btnMinimized.Text = "ــــ";
+            this.btnMinimized.UseVisualStyleBackColor = false;
+            this.btnMinimized.Click += new System.EventHandler(this.btnMinimized_Click);
+            // 
+            // btnMaximized
+            // 
+            this.btnMaximized.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnMaximized.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnMaximized.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnMaximized.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.btnMaximized.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.btnMaximized.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMaximized.Font = new System.Drawing.Font("MingLiU-ExtB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMaximized.ForeColor = System.Drawing.Color.Black;
+            this.btnMaximized.Location = new System.Drawing.Point(702, 0);
+            this.btnMaximized.Name = "btnMaximized";
+            this.btnMaximized.Size = new System.Drawing.Size(60, 40);
+            this.btnMaximized.TabIndex = 9;
+            this.btnMaximized.Text = "🗖";
+            this.btnMaximized.UseVisualStyleBackColor = false;
+            this.btnMaximized.Click += new System.EventHandler(this.btnMaximized_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("MV Boli", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(4, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 22);
+            this.label1.TabIndex = 100;
+            this.label1.Text = "Send Mail As PDF";
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnClose.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnClose.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("MingLiU-ExtB", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.Black;
+            this.btnClose.Location = new System.Drawing.Point(762, 0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(60, 40);
+            this.btnClose.TabIndex = 10;
+            this.btnClose.Text = "✖";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bell MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(43, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 30);
+            this.label2.TabIndex = 100;
+            this.label2.Text = "E-mail Address";
+            // 
+            // tbxEmailAddress
+            // 
+            this.tbxEmailAddress.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(254)))), ((int)(((byte)(243)))));
+            this.tbxEmailAddress.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbxEmailAddress.Location = new System.Drawing.Point(167, 76);
+            this.tbxEmailAddress.Name = "tbxEmailAddress";
+            this.tbxEmailAddress.Size = new System.Drawing.Size(387, 32);
+            this.tbxEmailAddress.TabIndex = 1;
+            this.tbxEmailAddress.Click += new System.EventHandler(this.tbxEmailAddress_Click);
+            this.tbxEmailAddress.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbxEmailAddress_KeyDown);
+            this.tbxEmailAddress.Leave += new System.EventHandler(this.tbxEmailAddress_Leave);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bell MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(62, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 30);
+            this.label3.TabIndex = 100;
+            this.label3.Text = "Mail Subject";
+            // 
+            // tbxSubject
+            // 
+            this.tbxSubject.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(254)))), ((int)(((byte)(243)))));
+            this.tbxSubject.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbxSubject.Location = new System.Drawing.Point(167, 118);
+            this.tbxSubject.Name = "tbxSubject";
+            this.tbxSubject.Size = new System.Drawing.Size(387, 32);
+            this.tbxSubject.TabIndex = 2;
+            this.tbxSubject.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbxSubject_KeyDown);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bell MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(43, 340);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(183, 30);
+            this.label4.TabIndex = 100;
+            this.label4.Text = "PDF File Name";
+            // 
+            // tbxPdfFileName
+            // 
+            this.tbxPdfFileName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(254)))), ((int)(((byte)(243)))));
+            this.tbxPdfFileName.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbxPdfFileName.Location = new System.Drawing.Point(167, 337);
+            this.tbxPdfFileName.Name = "tbxPdfFileName";
+            this.tbxPdfFileName.Size = new System.Drawing.Size(387, 32);
+            this.tbxPdfFileName.TabIndex = 4;
+            this.tbxPdfFileName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbxPdfFileName_KeyDown);
+            // 
+            // rtbMailMessage
+            // 
+            this.rtbMailMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbMailMessage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(254)))), ((int)(((byte)(243)))));
+            this.rtbMailMessage.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rtbMailMessage.Location = new System.Drawing.Point(167, 161);
+            this.rtbMailMessage.Name = "rtbMailMessage";
+            this.rtbMailMessage.Size = new System.Drawing.Size(620, 118);
+            this.rtbMailMessage.TabIndex = 3;
+            this.rtbMailMessage.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Bell MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(50, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(165, 30);
+            this.label5.TabIndex = 100;
+            this.label5.Text = "Mail Message";
+            // 
+            // rtbPdfText
+            // 
+            this.rtbPdfText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbPdfText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(254)))), ((int)(((byte)(243)))));
+            this.rtbPdfText.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rtbPdfText.Location = new System.Drawing.Point(33, 381);
+            this.rtbPdfText.Name = "rtbPdfText";
+            this.rtbPdfText.Size = new System.Drawing.Size(754, 258);
+            this.rtbPdfText.TabIndex = 5;
+            this.rtbPdfText.Text = "";
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.btnSend.FlatAppearance.BorderSize = 0;
+            this.btnSend.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.btnSend.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSend.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.ForeColor = System.Drawing.Color.BurlyWood;
+            this.btnSend.Location = new System.Drawing.Point(680, 70);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(107, 35);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.btnClear.FlatAppearance.BorderSize = 0;
+            this.btnClear.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.BurlyWood;
+            this.btnClear.Location = new System.Drawing.Point(680, 112);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(107, 35);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear All";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // dragControl1
+            // 
+            this.dragControl1.SelectControl = this.pnlHeader;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.BurlyWood;
+            this.ClientSize = new System.Drawing.Size(822, 676);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.rtbPdfText);
+            this.Controls.Add(this.rtbMailMessage);
+            this.Controls.Add(this.tbxPdfFileName);
+            this.Controls.Add(this.tbxSubject);
+            this.Controls.Add(this.tbxEmailAddress);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pnlHeader);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.pnlHeader.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClose;
+        private DragControl dragControl1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxEmailAddress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxSubject;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbxPdfFileName;
+        private System.Windows.Forms.RichTextBox rtbMailMessage;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox rtbPdfText;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnMinimized;
+        private System.Windows.Forms.Button btnMaximized;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,207 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace CreatePdfSendAsMail
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("The text you wrote will be sent as PDF. Do you confirm?","Confirm",MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    string pdfFilePath = @"C:\Users\" + SystemInformation.UserName + @"\Desktop\" + tbxPdfFileName.Text + ".pdf";
+
+                    SmtpClient client = new SmtpClient()
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential()
+                        {
+                            UserName = "arsene.lupin9901@gmail.com",
+                            Password = "1999202179"
+                        }
+                    };
+
+                    MailAddress fromEmail = new MailAddress("arsene.lupin9901@gmail.com", "Hakam Qalalwi");
+                    MailAddress toEmail = new MailAddress(tbxEmailAddress.Text);
+
+                    MailMessage mailMessage = new MailMessage()
+                    {
+                        From = fromEmail,
+                        Subject = tbxSubject.Text,
+                        Body = rtbMailMessage.Text
+                    };
+
+                    CreatePdf(pdfFilePath);
+
+                    mailMessage.Attachments.Add(new Attachment(pdfFilePath));
+                    mailMessage.To.Add(toEmail);
+
+
+                    //// syncronic /////////////////////////////////////////////////////////////
+                    //try
+                    //{
+                    //    client.Send(mailMessage);
+                    //    MessageBox.Show("Mail sent successfully.","Information",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                    //}
+                    //catch (Exception exception)
+                    //{
+                    //    MessageBox.Show("An error occurred!\n" + "Error message: " + exception.Message, "Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    //}
+                    ////////////////////////////////////////////////////////////////////////////
+
+
+                    //// asyncronic ///////
+                    client.SendMailAsync(mailMessage);
+                    client.SendCompleted += Client_SendCompleted;
+
+                    System.Threading.Thread.Sleep(2000);
+
+                    client.Dispose();
+                    mailMessage.Dispose();
+
+                    if (MessageBox.Show("Delete PDF file created on desktop?", "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        File.Delete(pdfFilePath);
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("An exception occurred while the application was running!\n" + "Exception message: " + exception.Message, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void ClearAllControls()
+        {
+            tbxEmailAddress.Text = "";
+            tbxPdfFileName.Text = "";
+            tbxSubject.Text = "";
+            rtbMailMessage.Text = "";
+            rtbPdfText.Text = "";
+        }
+
+        private void Client_SendCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show("An error occurred!\n" + "Error message: " + e.Error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+                MessageBox.Show("Mail sent successfully.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void CreatePdf(string filePath)
+        {
+            iTextSharp.text.Document pdfDoc = new iTextSharp.text.Document();
+            PdfWriter.GetInstance(pdfDoc, new FileStream(filePath, FileMode.Create, FileAccess.ReadWrite));
+
+            // For Turkish Characters
+            iTextSharp.text.pdf.BaseFont STF_Helvetica_Turkish = BaseFont.CreateFont("Helvetica", "CP1254", BaseFont.NOT_EMBEDDED);
+            iTextSharp.text.Font fontTitle = new iTextSharp.text.Font(STF_Helvetica_Turkish, 12, iTextSharp.text.Font.NORMAL);
+
+            pdfDoc.AddAuthor("FirstName Lastname");
+            pdfDoc.AddCreationDate();
+            pdfDoc.AddHeader(tbxPdfFileName.Text, tbxSubject.Text);
+            if (pdfDoc.IsOpen() == false)
+            {
+                pdfDoc.Open();
+            }
+            pdfDoc.Add(new Paragraph(rtbPdfText.Text, fontTitle));
+            pdfDoc.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearAllControls();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMaximized_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Normal;
+                return;
+            }
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void btnMinimized_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void tbxEmailAddress_Leave(object sender, EventArgs e)
+        {
+            string pattern = "^([0-9a-zA-Z]([-\\.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[0-9a-zA-Z]{2,9})$";
+            if (Regex.IsMatch(tbxEmailAddress.Text,pattern))
+            {
+                errorProvider1.Clear();
+            }
+            else
+            {
+                errorProvider1.SetError(this.tbxEmailAddress, "Incorrect email address entry");
+                tbxEmailAddress.ForeColor = Color.Red;
+                tbxEmailAddress.Text = "Please enter the e-mail address in the correct format.";
+            }
+        }
+
+        private void tbxEmailAddress_Click(object sender, EventArgs e)
+        {
+            tbxEmailAddress.Text = "";
+            tbxEmailAddress.ForeColor = Color.Black;
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            tbxEmailAddress.Focus();
+        }
+
+        private void tbxEmailAddress_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                tbxSubject.Focus();
+            }
+        }
+
+        private void tbxSubject_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                rtbMailMessage.Focus();
+            }
+        }
+
+        private void tbxPdfFileName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                rtbPdfText.Focus();
+            }
+        }
+    }
+}
